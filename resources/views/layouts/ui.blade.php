bf4fc89fbbf2a0feb4a325405374e69448a5d668
the-provost <mailstojesil@gmail.com>
2021-09-02 17:14:52 +0530

    Fixed UI

--- resources/views/layouts/ui.blade.php
+++ resources/views/layouts/ui.blade.php
@@ -72,8 +72,7 @@
   <footer class="main-footer shadow-none border-none border-top-0 accent-navy">
   <img src="images/icons/greymorelogosmall.png" alt="Greymore Logo" class="brand-image img-circle"
   style="opacity: 1">
-<span><img src="images/icons/greypass_small.png" alt="Greypass Logo" class="brand-image"
-style="opacity: 1"></span>
+{{-- <span><img src="images/icons/greypass_small.png" alt="Greypass Logo" class="brand-image"style="opacity: 1"></span> --}}
     <strong>&nbsp|&nbsp By <a href="http://www.greymore.tech" target="_blank"> Greymore Tech</a></strong>
 <div class="float-right d-none d-sm-inline">
         Vestibulum. Accommodare. Vincere.
