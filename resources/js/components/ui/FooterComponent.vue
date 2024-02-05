ba549a2cc4900661cc94e56c86c6559e9fc83156
bridge <jesil.kj@bridge-global.com>
2024-02-05 17:58:37 +0530

    Footer, Sidebar, Control Sidebar and Navbar changed to vue components

---
+++ resources/js/components/ui/FooterComponent.vue
@@ -0,0 +1,15 @@
+<template>
+<!-- Main Footer -->
+ <footer class="main-footer shadow-none border-none border-top-0 accent-navy">
+    <img :src=footerImagePath alt="App Logo" class="brand-image img-circle"
+    style="opacity: 1">
+  <!-- <span><img src="images/icons/greypass_small.png" alt="Greypass Logo" class="brand-image"style="opacity: 1"></span> -->
+      <strong>&nbsp|&nbsp By <a href="http://www.greymore.tech" target="_blank"> Company Name here</a></strong>
+  <div class="float-right d-none d-sm-inline">
+          Company Tagline here.
+          </div>
+  </footer>
+</template>
+  <script>
+  const footerImagePath = '/images/AdminLTELogo.png';
+  </script>
