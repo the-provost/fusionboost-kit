258e676fde3524758137deabdfe0c86904296b12
jesiljose-bridge <jesil.kj@bridge-global.com>
2024-03-04 17:09:08 +0530

    Adminlte Signup and Login screens

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,108 +1,80 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
   	<title>Authentication | Login Portal</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
-
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	
-	<link rel="stylesheet" type="text/css" href="{{ URL::asset('portal/css/style.css') }}" >
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
+    @vite(['resources/js/app.js'])
 
 	</head>
-	<body>
-	<section class="ftco-section">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-md-6 text-center mb-5">
-					{{-- <h2 class="heading-section">Login</h2> --}}
-				</div>
-			</div>
-			<div class="row justify-content-center">
-				<div class="col-md-12 col-lg-10">
-					<div class="wrap d-md-flex">
-						<div class="text-wrap p-4 p-lg-5 text-center d-flex align-items-center order-md-last">
-							<div class="text w-100">
-								<h2>Welcome to MonolithSPA</h2>
-								<p>Don't have an account?</p>
-								<a href="{{ route('register') }}" class="btn btn-white btn-outline-white">Sign Up</a>
-							</div>
-			      </div>
-						<div class="login-wrap p-4 p-lg-5">
-			      	<div class="d-flex">
-			      		<div class="w-100">
-			      			<h3 class="mb-4">Sign In</h3>
-			      		</div>
-								<div class="w-100">
-									<p class="social-media d-flex justify-content-end">
-										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
-										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
-									</p>
-								</div>
-			      	</div>
-							<form action="{{ route('login') }}" Method="POST" class="signin-form">
-                                @csrf
-			      		<div class="form-group mb-3">
-			      			<label class="label" for="name">Username</label>
-			      			<input class="input100 form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" type="email" name="email" id="email" placeholder="{{ __('E-Mail Address') }}" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                              <span class="focus-input100"></span>
-							{{-- <span class="symbol-input100">
-								<i class="fa fa-user" aria-hidden="true"></i>
-							</span> --}}
-							@if ($errors->has('email'))
-								<br>
-								<span class="invalid-feedback " role="alert">
+<body class="hold-transition login-page">
+<div class="login-box">
+    <div class="login-logo">
+        <a href="../../index2.html"><b>Admin</b>LTE</a>
+    </div>
+    <div class="card">
+        <div class="card-body login-card-body">
+            <p class="login-box-msg">Sign in to start</p>
+            <form action="{{ route('login') }}" method="POST">
+                @csrf
+                <div class="input-group mb-3">
+                    <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" type="email" name="email" id="email" placeholder="{{ __('E-Mail Address') }}" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                            @if ($errors->has('email'))
+                                <br>
+                                <span class="invalid-feedback " role="alert">
 								<strong>{{ $errors->first('email') }}</strong>
 								</span>
-							@endif
-			      		</div>
-		            <div class="form-group mb-3">
-		            	<label class="label" for="password">Password</label>
-                        <input class="input100 form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" id="password" type="password" name="password" placeholder="{{ __('Password') }}" required autocomplete="current-password">
-                        <span class="focus-input100"></span>
-                        {{-- <span class="symbol-input100">
-                            <i class="fa fa-lock" aria-hidden="true"></i>
-                        </span> --}}
-                        @if ($errors->has('password'))
-                            <br>
-                            <span class="invalid-feedback" role="alert">                                        
-                            <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
-		            </div>
-		            <div class="form-group">
-		            	<button type="submit" class="form-control btn btn-primary submit px-3">{{ __('Sign In') }}</button>
-		            </div>
-		            <div class="form-group d-md-flex">
-		            	<div class="w-50 text-left">
-			            	<label class="checkbox-wrap checkbox-primary mb-0">Remember Me
-									  <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-									  <span class="checkmark"></span>
-										</label>
-									</div>
-                                    @if (Route::has('password.request'))
-									<div class="w-50 text-md-right">
-										<a href="{{ route('password.request') }}">Forgot Password?</a>
-									</div>
-                                    @endif
-		            </div>
-		          </form>
-		        </div>
-		      </div>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	
-    <script src="{{ URL::asset('portal/js/jquery.min.js') }}"></script> 
-    <script src="{{ URL::asset('portal/js/popper.js') }}"></script>
-	<script src="{{ URL::asset('portal/js/bootstrap.min.js')}}"></script>
-    <script src="{{ URL::asset('portal/js/main.js') }}"></script>
-
-
-	</body>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" id="password" type="password" name="password" placeholder="{{ __('Password') }}" required autocomplete="current-password">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                            @if ($errors->has('password'))
+                                <br>
+                                <span class="invalid-feedback " role="alert">
+								<strong>{{ $errors->first('password') }}</strong>
+								</span>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-8">
+                        <div class="icheck-primary {{ old('remember') ? 'checked' : '' }}">
+                            <input id="remember" type="checkbox"> <label for="remember">Remember Me</label>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <button class="btn btn-primary btn-block" type="submit">{{ __('Sign In') }}</button>
+                    </div>
+                </div>
+            </form>
+            <div class="social-auth-links text-center mb-3">
+                <p>- OR -</p><a class="btn btn-block btn-primary" href="#"><i class="fab fa-facebook mr-2"></i> Sign in using Facebook</a> <a class="btn btn-block btn-danger" href="#"><i class="fab fa-google-plus mr-2"></i> Sign in using Google+</a>
+            </div>
+            @if (Route::has('password.request'))
+                <p class="mb-1"><a href="{{ route('password.request') }}">I forgot my password</a></p>
+            @endif
+            <p class="mb-0"><a class="text-center" href="{{ route('register') }}">Register a new membership</a></p>
+            <br><br>
+            <a href="{{ route('terms') }}" style="text-decoration: none !important;">Terms of use &nbsp; | &nbsp;</a>
+            <a href="{{ route('terms') }}" style="text-decoration: none !important;">Privacy policy</a>
+        </div>
+    </div>
+</div>
+</body>
+<script src="https://kit.fontawesome.com/9d179d39b9.js" crossorigin="anonymous"></script>
 </html>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,139 +1,124 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-  	<title>Authentication | Login Portal</title>
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
-
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	
-	<link rel="stylesheet" type="text/css" href="{{ URL::asset('portal/css/style.css') }}" >
-
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <title>AdminLTE 3 | Registration Page</title>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
+    @vite(['resources/js/app.js'])
 	</head>
-	<body>
-	<section class="ftco-section">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-md-6 text-center mb-2 mt-2">
-					{{-- <h2 class="heading-section">Login</h2> --}}
-				</div>
-			</div>
-			<div class="row justify-content-center">
-				<div class="col-md-12 col-lg-10">
-					<div class="wrap d-md-flex">
-						
-						<div class="login-wrap p-4 p-lg-5">
-			      	<div class="d-flex">
-			      		<div class="w-100">
-			      			<h3 class="mb-4">Register</h3>
-			      		</div>
-								<div class="w-100">
-									<p class="social-media d-flex justify-content-end">
-										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
-										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
-									</p>
-								</div>
-			      	</div>
-							<form action="{{ route('register') }}" method="POST" class="signin-form">
-                                @csrf
-			      		<div class="form-group mb-3">
 
-                            <label class="label" for="first_name">First Name</label>
-                            <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" placeholder="Enter First Name" autofocus>
 
-                                @error('first_name')
-                                    <span class="invalid-feedback" role="alert">
+<body class="hold-transition register-page">
+<div class="register-box">
+    <div class="register-logo">
+        <a href="../../index2.html"><b>Admin</b>LTE</a>
+    </div>
+    <div class="card">
+        <div class="card-body register-card-body">
+            <p class="login-box-msg">Register a new membership</p>
+            <form action="{{ route('register') }}" method="POST">
+                @csrf
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control @error('first_name') is-invalid @enderror" id="first_name" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" placeholder="Enter First Name">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-user"></span>
+                        </div>
+                        @error('first_name')
+                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-
-			      		</div>
-		            <div class="form-group mb-3">
-		            	<label class="label" for="last_name">Last Name</label>
-                            <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" placeholder="Enter Last Name" autofocus>
-
-                                @error('last_name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-		            </div>
-
-                    <div class="form-group mb-3">
-		            	<label class="label" for="email">Email</label>
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
                         @enderror
-		            </div>
-
-
-                    <div class="form-group mb-3">
-		            	<label class="label" for="phone">Phone</label>
-                        <input id="phone" type="phone" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone">
-
-                        @error('phone')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-		            </div>
-
-                    <div class="form-group mb-3">
-		            	<label class="label" for="password">Password</label>
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-		            </div>
-
-                    <div class="form-group mb-3">
-		            	<label class="label" for="password-confirm">Confirm Password</label>
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                           
-		            </div>
-
-
-		            <div class="form-group">
-		            	<button type="submit" class="form-control btn btn-primary submit px-3">{{ __('Sign Up') }}</button>
-		            </div>
-		            <br>
-                    <a href="{{ route('terms') }}">Terms of use &nbsp; | &nbsp;</a>
-                        <a href="{{ route('terms') }}">Privacy policy</a>
-		          </form>
-		        </div>
-
-                <div class="text-wrap p-4 p-lg-5 text-center d-flex align-items-center order-md-first">
-                    <div class="text w-100">
-                        <h2>Hello there!</h2>
-                        <p>Already have an account?</p>
-                        <a href="{{ route('login') }}" class="btn btn-white btn-outline-white">Sign In</a>
-                        <br>
-                        
                     </div>
-          </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control @error('last_name') is-invalid @enderror" id="last_name" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" placeholder="Enter Last Name">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-user"></span>
+                        </div>
+                        @error('last_name')
+                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Enter Email Address">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                        @enderror
+                    </div>
+                </div>
 
-		      </div>
-				</div>
-			</div>
-		</div>
-	</section>
+                <div class="input-group mb-3">
+                    <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone" placeholder="Enter Phone Number">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                        @error('phone')
+                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                        @enderror
+                    </div>
+                </div>
 
-	
-    <script src="{{ URL::asset('portal/js/jquery.min.js') }}"></script> 
-    <script src="{{ URL::asset('portal/js/popper.js') }}"></script>
-	<script src="{{ URL::asset('portal/js/bootstrap.min.js')}}"></script>
-    <script src="{{ URL::asset('portal/js/main.js') }}"></script>
+                <div class="input-group mb-3">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Enter New Password">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input class="form-control" placeholder="Retype Your Password" type="password">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-8">
+                        <div class="icheck-primary">
+                            <input id="agreeTerms" name="terms" type="checkbox" value="agree"> <label for="agreeTerms">I agree to the <a href="{{ route('terms') }}">terms</a></label>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <button class="btn btn-primary btn-block" type="submit">{{ __('Sign Up') }}</button>
+                    </div>
+                </div>
+            </form>
+            <div class="social-auth-links text-center">
+                <p>- OR -</p><a class="btn btn-block btn-primary" href="#"><i class="fab fa-facebook mr-2"></i> Sign up using Facebook</a> <a class="btn btn-block btn-danger" href="#"><i class="fab fa-google-plus mr-2"></i> Sign up using Google+</a>
+            </div>
+            <a class="text-center" href="{{ route('login') }}">I already have a membership</a>
+            <br><br>
+            <a href="{{ route('terms') }}" style="text-decoration: none !important;">Terms of use &nbsp; | &nbsp;</a>
+            <a href="{{ route('terms') }}" style="text-decoration: none !important;">Privacy policy</a>
+        </div>
+    </div>
+</div>
+</body>
+<script src="https://kit.fontawesome.com/9d179d39b9.js" crossorigin="anonymous"></script>
 
 
-	</body>
 </html>
 
